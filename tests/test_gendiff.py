5a73077b6b2d7dc9061025fac88e3447005bc480
etogit <sturik@bk.ru>
2023-04-10 17:43:45 +0300

    add tests

--- tests/test_gendiff.py
+++
@@ -1,6 +0,0 @@
-from gendiff.genarate import generate_diff
-
-coll = generate_diff(filename1, filename2)
-
-def test_generate_diff(coll):
-	assert 
---
+++ tests/tests.py
@@ -0,0 +1,13 @@
+from gendiff.genarate import generate_diff
+
+RIGHT_RESULT = {
+    "    host": "hexlet.io",
+    "  - timeout": "50",
+    "  + timeout": "20",
+    "  - follow": "false",
+    "  - proxy": "123.234.53.22",
+    "  + verbose": "true",
+  }
+
+def test_gendiff():
+  assert RIGHT_RESULT == generate_diff(file1.json, file2.json)
