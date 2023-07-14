92b5f36512f8988117e62466b967cf15bcb66782
etogit <sturik@bk.ru>
2023-07-14 17:18:22 +0300

    add tests

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Вычисление отличий
-### Вычислитель отличий – программа, которая определяет разницу между двумя структурами данных.
-### Это популярная задача, для решения которой существует множество онлайн-сервисов, например, jsondiff.
-### Подобный механизм, например, используется при выводе тестов или при автоматическом отслеживании изменений в конфигурационных файлах.
-### Возможности утилиты:
-###     - Поддержка разных входных форматов: yaml, json
-###     - Генерация отчета в виде plain text, stylish и json
-
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/KepiWole/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/KepiWole/python-project-50/actions)
-[![Maintainability](https://api.codeclimate.com/v1/badges/33549fdd1bb009697faa/maintainability)](https://codeclimate.com/github/KepiWole/python-project-50/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/33549fdd1bb009697faa/test_coverage)](https://codeclimate.com/github/KepiWole/python-project-50/test_coverage)
+[![Actions Status](https://github.com/KepiWole/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/>
+[![Maintainability](https://api.codeclimate.com/v1/badges/33549fdd1bb009697faa/maintainability)](https://codeclimate.co>
+[![Test Coverage](https://api.codeclimate.com/v1/badges/33549fdd1bb009697faa/test_coverage)](https://codeclimate.com/gi>
+
+# Вычисление отличий
+
+## Описание 
+Вычислитель отличий – программа, которая определяет разницу между двумя структурами данных.
+Это популярная задача, для решения которой существует множество онлайн-сервисов, например, jsondiff.
+Подобный механизм, например, используется при выводе тестов или при автоматическом отслеживании изменений в конфигурационных файлах.
+## Возможности утилиты:
+     - Поддержка разных входных форматов: yaml, json
+     - Генерация отчета в виде plain text, stylish и json
 
 ### Разница двух json файлов
 [![asiicast](https://asciinema.org/a/usGll7FLEdymjhf9SRh7Drwgh.png)](https://asciinema.org/a/usGll7FLEdymjhf9SRh7Drwgh)
@@ -22,3 +24,4 @@
 
 ### Вывод в JSON
 [![asiicast](https://asciinema.org/a/D0U2ndgibEAvwchWxXabQjHAr.png)](https://asciinema.org/a/D0U2ndgibEAvwchWxXabQjHAr)
+
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,4 @@
+import pytest
 from gendiff.generate import generate_diff
 from tests.fixtures.expected_result import PLAIN_DIFF_STR_12
 from tests.fixtures.expected_result import PLAIN_DIFF_STR_21
@@ -5,101 +6,38 @@ from tests.fixtures.expected_result import NESTED_PLAIN_DIFF_STR_12
 from tests.fixtures.expected_result import NESTED_STYLISH_DIFF_STR_12
 
 
-def test_gendiff_stylish_plain():
+@pytest.mark.paramerize("test_generate_diff,expected", [(('./tests/fixtures/plain1.json',
+			'./tests/fixtures/plain2.json'),PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/plain2.json','./tests/fixtures/plain1.json','stylish'),
+			PLAIN_DIFF_STR_21),
+			(('./tests/fixtures/plain1.yaml','./tests/fixtures/plain2.yaml','stylish'),
+			PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/plain1.json','./tests/fixtures/plain2.yaml','stylish'),
+			PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/plain1.yml','./tests/fixtures/plain2.json','stylish'),
+			PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/plain2.yml','./tests/fixtures/plain1.yml','stylish'),
+			PLAIN_DIFF_STR_21),
+			(('./tests/fixtures/plain2.yml','./tests/fixtures/plain1.json','stylish'),
+			PLAIN_DIFF_STR_21),
+			(('./tests/fixtures/plain2.json','./tests/fixtures/plain1.yaml','stylish'),
+			PLAIN_DIFF_STR_21),
+			(('./tests/fixtures/nested1.json','./tests/fixtures/nested2.json','stylish'),
+			NESTED_STYLISH_DIFF_STR_12),
+			(('./tests/fixtures/nested1.yaml','./tests/fixtures/nested2.yaml','stylish'),
+			NESTED_STYLISH_DIFF_STR_12),
+			(('./tests/fixtures/nested1.json','./tests/fixtures/nested2.yaml','stylish'),
+			NESTED_STYLISH_DIFF_STR_12),
+			(('./tests/fixtures/nested1.yaml','./tests/fixtures/nested2.json','stylish'),
+			NESTED_STYLISH_DIFF_STR_12),
+			(('./tests/fixtures/nested1.json','./tests/fixtures/nested2.json','plain'),
+			NESTED_PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/nested1.yaml','./tests/fixtures/nested2.yaml','plain'),
+			NESTED_PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/nested1.json','./tests/fixtures/nested2.yaml','plain'),
+			NESTED_PLAIN_DIFF_STR_12),
+			(('./tests/fixtures/nested1.yaml','./tests/fixtures/nested2.json','plain'),
+			NESTED_PLAIN_DIFF_STR_12)]
 
-    assert generate_diff(
-        './tests/fixtures/plain1.json',
-        './tests/fixtures/plain2.json',
-        'stylish'
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain2.json',
-        './tests/fixtures/plain1.json',
-        'stylish'
-    ) == PLAIN_DIFF_STR_21
-
-    assert generate_diff(
-        './tests/fixtures/plain1.yaml',
-        './tests/fixtures/plain2.yaml',
-        'stylish'
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain1.json',
-        './tests/fixtures/plain2.yaml',
-        'stylish'
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain1.yml',
-        './tests/fixtures/plain2.json',
-        'stylish'
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain2.yml',
-        './tests/fixtures/plain1.yml',
-        'stylish'
-    ) == PLAIN_DIFF_STR_21
-
-    assert generate_diff(
-        './tests/fixtures/plain2.yml',
-        './tests/fixtures/plain1.json',
-        'stylish'
-    ) == PLAIN_DIFF_STR_21
-
-    assert generate_diff(
-        './tests/fixtures/plain2.json',
-        './tests/fixtures/plain1.yaml',
-        'stylish'
-    ) == PLAIN_DIFF_STR_21
-
-
-def test_gendiff_stylish_nested():
-    assert generate_diff(
-        './tests/fixtures/nested1.json',
-        './tests/fixtures/nested2.json',
-        'stylish'
-    ) == NESTED_STYLISH_DIFF_STR_12
-    assert generate_diff(
-        './tests/fixtures/nested1.yaml',
-        './tests/fixtures/nested2.yaml',
-        'stylish'
-    ) == NESTED_STYLISH_DIFF_STR_12
-    assert generate_diff(
-        './tests/fixtures/nested1.json',
-        './tests/fixtures/nested2.yaml',
-        'stylish'
-    ) == NESTED_STYLISH_DIFF_STR_12
-    assert generate_diff(
-        './tests/fixtures/nested1.yaml',
-        './tests/fixtures/nested2.json',
-        'stylish'
-    ) == NESTED_STYLISH_DIFF_STR_12
-
-
-def test_gendiff_plain_nested():
-    assert generate_diff(
-        './tests/fixtures/nested1.json',
-        './tests/fixtures/nested2.json',
-        'plain'
-    ) == NESTED_PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/nested1.yaml',
-        './tests/fixtures/nested2.yaml',
-        'plain'
-    ) == NESTED_PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/nested1.json',
-        './tests/fixtures/nested2.yaml',
-        'plain'
-    ) == NESTED_PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/nested1.yaml',
-        './tests/fixtures/nested2.json',
-        'plain'
-    ) == NESTED_PLAIN_DIFF_STR_12
+def test_gendiff(generate_diff, expected):
+	assert generate_diff(test_generate_diff) == expected
